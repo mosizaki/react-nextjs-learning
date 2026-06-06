d3aa97e092f3669ed75ae3dd1405cb882c359a76
mosizaki <ma.webdd@gmail.com>
2026-06-06 21:24:07 +0330

    updated script.js file

---
+++ day-01-js-refresh/script.js
@@ -0,0 +1,30 @@
+const name = "mosi"
+let age = 25
+
+const name2 = "michael"
+let age = 20
+
+function message (userName) {
+    return `hello my name is ${userName}`
+}
+
+const greetOthers = (userName) => {
+    return `hello this is an arrow function and i'm ${userName}`
+}
+
+const user = {
+    name: 'carl',
+    age: 22,
+    massege(userName) {
+        console.log(`hello my name is ${userName}`)
+    }
+}
+
+const skills = ['html', 'javascript', 'css']
+
+const user = {
+    name: 'mosi',
+    age: 30,
+    role: 'frontend developer',
+    skills: ['html', 'javascript', 'css']
+}
